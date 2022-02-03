d6b47b28cee6fc01e8810eb46c902d109c96858c
yuyare97 <yuya.take1020@gmail.com>
2022-02-03 17:23:51 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :move_to_edit, only: [:edit, :update, :destroy]
 
@@ -53,7 +54,11 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def move_to_edit
-      redirect_to root_path unless current_user == @prototype_user
+      redirect_to root_path unless current_user == @prototype.user
   end
 end
